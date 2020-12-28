b8b141475b98512f9062a88588f6741bc4b13bd4
comprelzhang <comprelzhang@163.com>
2020-12-28 20:02:17 +0800

    [add]

--- apps/api/configer/resource.py
+++ apps/api/configer/resource.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
 import datetime
 import json
 from lib.uuid_util import get_uuid
 from apps.background.models.dbserver import ResourceManager
 from apps.background.resource.configr.resource import ResourceObject
 
-class ResourceApi(ResourceManager):
+class ResourceApi(object):
     pass
 
