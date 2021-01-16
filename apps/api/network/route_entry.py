7836f78a09716323b62377bbb2c1d1625aff8066
comprelzhang <comprelzhang@163.com>
2021-01-16 15:47:54 +0800

    [update] route entry

--- apps/api/network/route_entry.py
+++ apps/api/network/route_entry.py
@@ -6,11 +6,11 @@ import traceback
 from lib.logs import logger
 from lib.json_helper import format_json_dumps
 from apps.common.convert_keys import define_relations_key
+from apps.api.apibase import ApiBase
 from apps.api.configer.provider import ProviderApi
 from apps.background.resource.network.vpc import VpcObject
 from apps.background.resource.network.route_table import RouteTableObject
 from apps.background.resource.network.route_entry import RouteEntryObject
-from apps.api.apibase import ApiBase
 
 
 class RouteEntryApi(ApiBase):
@@ -21,14 +21,30 @@ class RouteEntryApi(ApiBase):
         self.resource_object = RouteEntryObject()
         self.resource_keys_config = None
 
-    def formate_result(self, result):
-        return result
-
     def save_data(self, rid, name, vpc, destination,
                   route_table, next_type, next_hub,
                   provider, provider_id, region, zone,
                   extend_info, define_json,
                   status, result_json):
+        '''
+
+        :param rid:
+        :param name:
+        :param vpc:
+        :param destination:
+        :param route_table:
+        :param next_type:
+        :param next_hub:
+        :param provider:
+        :param provider_id:
+        :param region:
+        :param zone:
+        :param extend_info:
+        :param define_json:
+        :param status:
+        :param result_json:
+        :return:
+        '''
 
         self.resource_object.create(create_data={"id": rid, "provider": provider,
                                                  "provider_id": provider_id,
@@ -44,6 +60,14 @@ class RouteEntryApi(ApiBase):
                                                  "result_json": json.dumps(result_json)})
 
     def before_keys_checks(self, provider, vpc_id, route_table_id):
+        '''
+
+        :param provider:
+        :param vpc_id:
+        :param route_table_id:
+        :return:
+        '''
+
         self.resource_info(provider)
         resource_property = self.resource_keys_config["resource_property"]
         _vpc_status = define_relations_key("vpc_id", vpc_id, resource_property.get("vpc_id"))
@@ -58,36 +82,43 @@ class RouteEntryApi(ApiBase):
         return ext_info
 
     def create(self, rid, name, provider_id, zone, region,
-               vpc_id, route_table, next_type, next_hub, destination,
-               extend_info, **kwargs):
+               vpc_id, route_table, next_type, next_hub,
+               destination, extend_info):
+
         '''
 
         :param rid:
         :param name:
-        :param cidr:
         :param provider_id:
+        :param zone:
+        :param region:
+        :param vpc_id:
+        :param route_table:
+        :param next_type:
+        :param next_hub:
+        :param destination:
         :param extend_info:
-        :param kwargs:
         :return:
         '''
+
         # todo 依据不同的next type转化不同的id
-
         extend_info = extend_info or {}
-        provider_object, provider_info = ProviderApi().provider_info(provider_id, region)
-
-        _relations_id_dict = self.before_keys_checks(provider_object["name"], vpc_id, route_table)
-
+        label_name = self.resource_name + "_" + rid
         create_data = {"name": name,
                        "destination": destination,
                        "next_type": next_type,
                        "next_hub": next_hub}
 
+        provider_object, provider_info = ProviderApi().provider_info(provider_id, region)
+        _relations_id_dict = self.before_keys_checks(provider_object["name"], vpc_id, route_table)
+
         create_data.update(_relations_id_dict)
-
         define_json = self._generate_resource(provider_object["name"], rid,
-                                          data=create_data, extend_info=extend_info)
+                                              data=create_data, extend_info=extend_info)
 
+        output_json = self._generate_output(label_name=label_name)
         define_json.update(provider_info)
+        define_json.update(output_json)
 
         _path = self.create_workpath(rid,
                                      provider=provider_object["name"],
@@ -109,13 +140,13 @@ class RouteEntryApi(ApiBase):
 
         result = self.formate_result(result)
         logger.info(format_json_dumps(result))
-        resource_id = self._fetch_id(result)
 
-        _update_data = {"status": "ok",
-                        "resource_id": resource_id,
-                        "result_json": format_json_dumps(result)}
+        _update_data = {"status": "ok", "result_json": format_json_dumps(result)}
         _update_data.update(self._read_output_result(result))
+
+        if not _update_data.get("resource_id"):
+            _update_data["resource_id"] = self._fetch_id(result)
+
         self.update_data(rid, data=_update_data)
 
         return rid
-
