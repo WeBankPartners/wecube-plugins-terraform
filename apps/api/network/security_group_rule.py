045bf4f960af7aa1172f1e084f71ac5362357c14
comprelzhang <comprelzhang@163.com>
2021-01-16 16:04:00 +0800

    [update] sg rule

--- apps/api/network/security_group_rule.py
+++ apps/api/network/security_group_rule.py
@@ -5,6 +5,7 @@ import json
 import traceback
 from lib.logs import logger
 from lib.json_helper import format_json_dumps
+from apps.common.convert_keys import define_relations_key
 from apps.api.apibase import ApiBase
 from apps.api.configer.provider import ProviderApi
 from apps.background.resource.network.security_group import SecGroupObject
@@ -19,8 +20,25 @@ class SecGroupRuleApi(ApiBase):
         self.resource_object = SecGroupRuleObject()
         self.resource_keys_config = None
 
-    def formate_result(self, result):
-        return result
+    def before_keys_checks(self, provider, security_group_id):
+        '''
+
+        :param provider:
+        :param security_group_id:
+        :return:
+        '''
+
+        self.resource_info(provider)
+        resource_property = self.resource_keys_config["resource_property"]
+        _sg_status = define_relations_key("security_group_id", security_group_id,
+                                          resource_property.get("security_group_id"))
+
+        ext_info = {}
+        if security_group_id and (not _sg_status):
+            ext_info["security_group_id"] = SecGroupObject().resource_id(security_group_id)
+
+        logger.info("before_keys_checks add info: %s" % (format_json_dumps(ext_info)))
+        return ext_info
 
     def save_data(self, rid, name, security_group_id,
                   cidr_ip, ip_protocol, type,
@@ -28,7 +46,28 @@ class SecGroupRuleApi(ApiBase):
                   provider, provider_id, region, zone,
                   extend_info, define_json,
                   status, result_json):
-        description = description or "%s_%s_%s" % (type, ip_protocol, ports)
+        '''
+
+        :param rid:
+        :param name:
+        :param security_group_id:
+        :param cidr_ip:
+        :param ip_protocol:
+        :param type:
+        :param description:
+        :param ports:
+        :param policy:
+        :param provider:
+        :param provider_id:
+        :param region:
+        :param zone:
+        :param extend_info:
+        :param define_json:
+        :param status:
+        :param result_json:
+        :return:
+        '''
+
         self.resource_object.create(create_data={"id": rid, "provider": provider,
                                                  "region": region, "zone": zone,
                                                  "security_group_id": security_group_id,
@@ -45,38 +84,45 @@ class SecGroupRuleApi(ApiBase):
                security_group_id, type,
                cidr_ip, ip_protocol,
                ports, policy, description,
-               zone, region, extend_info, **kwargs):
+               zone, region, extend_info):
         '''
 
         :param rid:
+        :param name:
+        :param provider_id:
         :param security_group_id:
         :param type:
         :param cidr_ip:
         :param ip_protocol:
         :param ports:
         :param policy:
+        :param description:
         :param zone:
         :param region:
         :param extend_info:
-        :param kwargs:
         :return:
         '''
 
-        secGroup_object_id = SecGroupObject().resource_id(security_group_id)
+        extend_info = extend_info or {}
+        label_name = self.resource_name + "_" + rid
 
-        provider_object, provider_info = ProviderApi().provider_info(provider_id, region)
-
+        description = description or "%s_%s_%s" % (type, ip_protocol, ports)
         create_data = {"description": description,
-                       "security_group_id": secGroup_object_id,
                        "type": type, "ports": ports,
                        "cidr_ip": cidr_ip,
                        "ip_protocol": ip_protocol,
                        "policy": policy}
 
-        define_json = self._generate_resource(provider_object["name"], rid,
-                                          data=create_data, extend_info=extend_info)
+        provider_object, provider_info = ProviderApi().provider_info(provider_id, region)
+        _relations_id_dict = self.before_keys_checks(provider_object["name"], security_group_id)
 
+        create_data.update(_relations_id_dict)
+        define_json = self._generate_resource(provider_object["name"], label_name=label_name,
+                                              data=create_data, extend_info=extend_info)
+
+        output_json = self._generate_output(label_name=label_name)
         define_json.update(provider_info)
+        define_json.update(output_json)
 
         _path = self.create_workpath(rid,
                                      provider=provider_object["name"],
@@ -99,12 +145,13 @@ class SecGroupRuleApi(ApiBase):
 
         result = self.formate_result(result)
         logger.info(format_json_dumps(result))
-        resource_id = self._fetch_id(result)
 
-        _update_data = {"status": "ok",
-                        "resource_id": resource_id[:36],
-                        "result_json": format_json_dumps(result)}
+        _update_data = {"status": "ok", "result_json": format_json_dumps(result)}
         _update_data.update(self._read_output_result(result))
+
+        if not _update_data.get("resource_id"):
+            _update_data["resource_id"] = self._fetch_id(result)
+
         self.update_data(rid, data=_update_data)
 
         return rid
