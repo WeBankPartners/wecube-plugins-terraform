ca84597c121f887e5f57df27968a4ffc1754cc03
comprelzhang <comprelzhang@163.com>
2021-01-19 15:21:02 +0800

    [add]  ccn api

--- apps/api/network/connnect_network.py
+++ apps/api/network/connnect_network.py
@@ -1,141 +1,116 @@
 # coding: utf-8
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
-import os
 import json
 import traceback
-from core import local_exceptions
-from lib.json_helper import format_json_dumps
 from lib.logs import logger
-from lib.uuid_util import get_uuid
-from wecube_plugins_terraform.settings import TERRAFORM_BASE_PATH
-from apps.common.generate import generate_data
+from lib.json_helper import format_json_dumps
+from core import local_exceptions
 from apps.api.configer.provider import ProviderApi
-from apps.api.configer.resource import ResourceObject
-from apps.api.configer.value_config import ValueConfigObject
-from apps.background.lib.commander.terraform import TerraformDriver
-from apps.background.resource.network.connnect_network import ConnectNetObject
-from apps.background.resource.network.connnect_network import ConnectNetAttachObject
+from apps.api.apibase import ApiBase
+from apps.background.resource.network.connnect_network import CCNObject
 
 
-class ConnectNetApi(object):
-    terraformDriver = TerraformDriver()
+class CCNApi(ApiBase):
+    def __init__(self):
+        super(CCNApi, self).__init__()
+        self.resource_name = "ccn"
+        self.resource_workspace = "ccn"
+        self.resource_object = CCNObject()
+        self.resource_keys_config = None
 
-    def create_workpath(self, rid, provider, region, zone):
-        zone = zone or ""
-        _az = "%s_%s" % (region, zone) if zone else region
-        _path = os.path.join(TERRAFORM_BASE_PATH, provider, _az, "vpc", rid)
-        if not os.path.exists(_path):
-            os.makedirs(_path)
+    def before_keys_checks(self, provider):
+        '''
 
-        self.terraformDriver.init_resource_dir(dir_path=_path, provider=provider)
-        return _path
+        :param provider:
+        :return:
+        '''
 
-    def write_define(self, rid, path, define_json):
-        with open(os.path.join(path, "%s.tf.json" % rid), 'wb+') as f:
-            json.dump(define_json, f, ensure_ascii=False, indent=4)
+        self.resource_info(provider)
+        return {}
 
-    def run(self, path):
-        self.terraformDriver.apply(path, auto_approve="")
-        return self.terraformDriver.resource_result(path)
-
-    def resource_info(self, provider):
-        _vpc_resource = ResourceObject().query_one(where_data={"provider": provider,
-                                                               "resource_name": "vpc"})
-        if not _vpc_resource:
-            raise local_exceptions.ResourceConfigError("vpc 资源未初始化完成配置")
-
-        return _vpc_resource
-
-    def resource_config(self, provider):
-        return ValueConfigObject().query_one(where_data={"provider": provider,
-                                                         "resource": "vpc"})
-
-    def revert(self, data, resource_property, data_modes):
-        result = {}
-        for k, v in data.items():
-            key = resource_property.get(k, k)
-            result[key] = generate_data(v, model=data_modes.get(v, {}))
-
-        return result
-
-    def _generate_vpc(self, provider, name, data):
-        _vpc_resource = self.resource_info(provider)
-        _vpc_config = self.resource_config(provider)
-
-        resource_name = _vpc_resource["resource_name"]
-        resource_property = _vpc_resource["resource_property"]
-
-        _columns = self.revert(data, resource_property=json.loads(resource_property),
-                               data_modes=json.loads(_vpc_config.get("value_config", "{}")))
-        _info = {
-            "resource": {
-                resource_name: {
-                    name: _columns
-                }
-            }
-        }
-        logger.info(format_json_dumps(_info))
-        return _info
-
-    def formate_result(self, result):
-        # todo 获取vpc创建信息
-        return result
-
-    def save_data(self, rid, name, provider, region, zone,
-                  cidr, extend_info, define_json,
+    def save_data(self, rid, name,
+                  provider, provider_id, region, zone,
+                  extend_info, define_json,
                   status, result_json):
-        ConnectNetObject().create(create_data={"id": rid, "provider": provider,
-                                        "region": region, "zone": zone,
-                                        "name": name, "cidr": cidr,
-                                        "status": status,
-                                        "extend_info": json.dumps(extend_info),
-                                        "define_json": json.dumps(define_json),
-                                        "result_json": result_json})
+        '''
 
-    def update_data(self, rid, data):
-        ConnectNetObject().update(rid, data)
+        :param rid:
+        :param name:
+        :param provider:
+        :param provider_id:
+        :param region:
+        :param zone:
+        :param cidr:
+        :param extend_info:
+        :param define_json:
+        :param vpc:
+        :param status:
+        :param result_json:
+        :return:
+        '''
 
-    def _fetch_id(self, result):
-        try:
-            _data = result.get("resources")[0]
-            _instances = _data.get("instances")[0]
-            _attributes = _instances.get("attributes")
-            return _attributes["id"]
-        except:
-            logger.info(traceback.format_exc())
-            raise ValueError("result can not fetch id")
+        self.resource_object.create(create_data={"id": rid, "provider": provider,
+                                                 "region": region, "zone": zone,
+                                                 "name": name, "status": status,
+                                                 "provider_id": provider_id,
+                                                 "extend_info": json.dumps(extend_info),
+                                                 "define_json": json.dumps(define_json),
+                                                 "result_json": json.dumps(result_json)})
 
-    def create(self, data):
-        name = data["name"]
-        cidr = data["cidr"]
-        provider = data["provider"]
-        region = data["region"]
-        zone = data.get("zone")
-        extend_info = data.get("extend_info", {})
-        rid = data.get("id") or get_uuid()
+    def create(self, rid, name, provider_id,
+               region, zone, extend_info):
+        '''
 
-        create_data = {"cidr": cidr, "name": name}
-        create_data.update(extend_info)
+        :param rid:
+        :param name:
+        :param provider_id:
+        :param region:
+        :param zone:
+        :param extend_info:
+        :return:
+        '''
 
-        _path = self.create_workpath(rid, provider, region, zone)
+        extend_info = extend_info or {}
+        create_data = {"name": name}
+        label_name = self.resource_name + "_" + rid
 
-        provider_info = ProviderApi().provider_info(provider, region=data.get("region"))
-        vpc_info = self._generate_vpc(provider, name, data=create_data)
-        vpc_info.update(provider_info)
+        provider_object, provider_info = ProviderApi().provider_info(provider_id, region)
+        _relations_id_dict = self.before_keys_checks(provider_object["name"])
 
-        self.save_data(rid, name=name, provider=provider, region=region,
-                       zone=zone, cidr=cidr, extend_info=extend_info,
-                       define_json=vpc_info, status="applying", result_json='{}')
+        create_data.update(_relations_id_dict)
+        define_json = self._generate_resource(provider_object["name"],
+                                              label_name=label_name,
+                                              data=create_data, extend_info=extend_info)
 
-        self.write_define(rid, _path, define_json=vpc_info)
+        output_json = self._generate_output(label_name=label_name)
+        define_json.update(provider_info)
+        define_json.update(output_json)
+
+        _path = self.create_workpath(rid,
+                                     provider=provider_object["name"],
+                                     region=region)
+
+        self.save_data(rid, name=name,
+                       provider=provider_object["name"],
+                       provider_id=provider_id,
+                       region=region, zone=zone,
+                       extend_info=extend_info,
+                       define_json=define_json,
+                       status="applying", result_json={})
+
+        self.write_define(rid, _path, define_json=define_json)
         result = self.run(_path)
 
         result = self.formate_result(result)
         logger.info(format_json_dumps(result))
-        resource_id = self._fetch_id(result)
-        self.update_data(rid, data={"status": "ok",
-                                    "resource_id": resource_id,
-                                    "result_json": format_json_dumps(result)})
+
+        _update_data = {"status": "ok", "result_json": format_json_dumps(result)}
+        _update_data.update(self._read_output_result(result))
+
+        if not _update_data.get("resource_id"):
+            _update_data["resource_id"] = self._fetch_id(result)
+
+        self.update_data(rid, data=_update_data)
 
         return rid
-
