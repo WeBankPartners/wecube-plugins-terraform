baa6d11042de80aa27ff544cf91fc966be3c84c9
comprelzhang <comprelzhang@163.com>
2021-01-20 20:41:31 +0800

    [add]  mmysql database

---
+++ apps/api/database/mysql/database.py
@@ -0,0 +1,135 @@
+# coding: utf-8
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+import base64
+import json
+from core import local_exceptions
+from lib.logs import logger
+from lib.json_helper import format_json_dumps
+from apps.api.configer.provider import ProviderApi
+from apps.common.convert_keys import define_relations_key
+from apps.background.resource.database.rds import MysqlObject
+from apps.background.resource.database.rds import MysqlDatabaseObject
+from apps.api.apibase import ApiBase
+
+
+class MysqlDatabaseApi(ApiBase):
+    def __init__(self):
+        super(MysqlDatabaseApi, self).__init__()
+        self.resource_name = "mysql"
+        self.resource_workspace = "mysql_database"
+        self.resource_object = MysqlDatabaseObject()
+        self.resource_keys_config = None
+
+    def before_keys_checks(self, provider, mysql_id):
+        '''
+
+        :param provider:
+        :param mysql_id:
+        :return:
+        '''
+
+        self.resource_info(provider)
+        resource_property = self.resource_keys_config["resource_property"]
+        _mysql_status = define_relations_key("mysql_id", mysql_id, resource_property.get("mysql_id"))
+
+        ext_info = {}
+        if mysql_id and (not _mysql_status):
+            ext_info["mysql_id"] = MysqlObject().object_resource_id(mysql_id)
+
+        logger.info("before_keys_checks add info: %s" % (format_json_dumps(ext_info)))
+        return ext_info
+
+    def save_data(self, rid, mysql_id, name,
+                  provider, region, extend_info, define_json,
+                  status, result_json):
+        '''
+
+        :param rid:
+        :param mysql_id:
+        :param name:
+        :param password:
+        :param provider:
+        :param region:
+        :param extend_info:
+        :param define_json:
+        :param status:
+        :param result_json:
+        :return:
+        '''
+
+        self.resource_object.create(create_data={"id": rid, "provider": provider,
+                                                 "region": region,
+                                                 "engine": self.resource_name,
+                                                 "rds_id": mysql_id,
+                                                 "name": name,
+                                                 "status": status,
+                                                 "extend_info": json.dumps(extend_info),
+                                                 "define_json": json.dumps(define_json),
+                                                 "result_json": json.dumps(result_json)})
+
+    def check_database(self, mysql_id, database):
+        _exists_data = MysqlDatabaseObject().query_one(where_data={"name": database, "rds_id": mysql_id})
+        if _exists_data:
+            raise local_exceptions.ResourceValidateError("database name", "database %s already exists" % database)
+
+    def create(self, rid, name, provider_id, mysql_id,
+               zone, region, extend_info, **kwargs):
+
+        '''
+
+        :param rid:
+        :param name:
+        :param provider_id:
+        :param mysql_id:
+        :param zone:
+        :param region:
+        :param extend_info:
+        :param kwargs:
+        :return:
+        '''
+
+        extend_info = extend_info or {}
+        label_name = self.resource_name + "_" + rid
+        create_data = {"name": name}
+
+        self.check_database(mysql_id, name)
+        provider_object, provider_info = ProviderApi().provider_info(provider_id, region)
+        _relations_id_dict = self.before_keys_checks(provider_object["name"], mysql_id)
+
+        create_data.update(_relations_id_dict)
+        define_json = self._generate_resource(provider_object["name"],
+                                              label_name=label_name,
+                                              data=create_data,
+                                              extend_info=extend_info)
+
+        output_json = self._generate_output(label_name=label_name)
+        define_json.update(provider_info)
+        define_json.update(output_json)
+
+        _path = self.create_workpath(rid,
+                                     provider=provider_object["name"],
+                                     region=region)
+
+        self.save_data(rid, region=region,
+                       name=name, mysql_id=mysql_id,
+                       provider=provider_object["name"],
+                       extend_info=extend_info,
+                       define_json=define_json,
+                       status="applying", result_json={})
+
+        self.write_define(rid, _path, define_json=define_json)
+        result = self.run(_path)
+
+        result = self.formate_result(result)
+        logger.info(format_json_dumps(result))
+
+        _update_data = {"status": "ok", "result_json": format_json_dumps(result)}
+        _update_data.update(self._read_output_result(result))
+
+        if not _update_data.get("resource_id"):
+            _update_data["resource_id"] = self._fetch_id(result)
+
+        self.update_data(rid, data=_update_data)
+        return rid
---
+++ apps/controller/database/mysql_controller/database.py
@@ -0,0 +1,138 @@
+# _ coding:utf-8 _*_
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+from core import validation
+from core import local_exceptions
+from core.controller import BackendController
+from core.controller import BackendIdController
+from core.controller import BaseController
+from lib.uuid_util import get_uuid
+from apps.api.database.mysql.database import MysqlDatabaseApi
+
+
+class MysqlDatabaseController(BackendController):
+    allow_methods = ('GET', 'POST')
+    resource = MysqlDatabaseApi()
+
+    def list(self, request, data, orderby=None, page=None, pagesize=None, **kwargs):
+        '''
+
+        :param request:
+        :param data:
+        :param orderby:
+        :param page:
+        :param pagesize:
+        :param kwargs:
+        :return:
+        '''
+
+        validation.allowed_key(data, ["id", "provider", "region", "rds_id", "name", "enabled"])
+
+        return self.resource.resource_object.list(filters=data, page=page,
+                                                  pagesize=pagesize, orderby=orderby)
+
+    def before_handler(self, request, data, **kwargs):
+        validation.allowed_key(data, ["id", "name", "provider_id", "mysql_id",
+                                      "zone", "region", "extend_info"])
+        validation.not_allowed_null(data=data,
+                                    keys=["region", "provider_id", "name", "mysql_id"]
+                                    )
+
+        validation.validate_string("id", data.get("id"))
+        validation.validate_string("name", data["name"])
+        validation.validate_string("region", data["region"])
+        validation.validate_string("zone", data.get("zone"))
+        validation.validate_string("mysql_id", data["mysql_id"])
+        validation.validate_string("provider_id", data.get("provider_id"))
+        validation.validate_dict("extend_info", data.get("extend_info"))
+
+    def create(self, request, data, **kwargs):
+        rid = data.pop("id", None) or get_uuid()
+        name = data.pop("name", None)
+        zone = data.pop("zone", None)
+        region = data.pop("region", None)
+        mysql_id = data.pop("mysql_id", None)
+        provider_id = data.pop("provider_id", None)
+        extend_info = validation.validate_dict("extend_info", data.pop("extend_info", None))
+
+        data.update(extend_info)
+        result = self.resource.create(rid, name, provider_id, mysql_id,
+                                      zone, region, extend_info=data)
+
+        return 1, result
+
+
+class MysqlDatabaseIdController(BackendIdController):
+    allow_methods = ('GET', 'DELETE',)
+    resource = MysqlDatabaseApi()
+
+    def show(self, request, data, **kwargs):
+        '''
+
+        :param request:
+        :param data:
+        :param kwargs:
+        :return:
+        '''
+
+        rid = kwargs.pop("rid", None)
+        return self.resource.resource_object.show(rid)
+
+    def delete(self, request, data, **kwargs):
+        rid = kwargs.pop("rid", None)
+        return self.resource.destory(rid)
+
+
+class MysqlDatabaseAddController(BaseController):
+    allow_methods = ("POST",)
+    resource = MysqlDatabaseApi()
+
+    def before_handler(self, request, data, **kwargs):
+        validation.not_allowed_null(data=data,
+                                    keys=["region", "provider_id", "name", "mysql_id"]
+                                    )
+
+        validation.validate_string("id", data.get("id"))
+        validation.validate_string("name", data["name"])
+        validation.validate_string("region", data["region"])
+        validation.validate_string("zone", data.get("zone"))
+        validation.validate_string("mysql_id", data["mysql_id"])
+        validation.validate_string("provider_id", data.get("provider_id"))
+
+    def response_templete(self, data):
+        return {}
+
+    def main_response(self, request, data, **kwargs):
+        rid = data.pop("id", None) or get_uuid()
+        name = data.pop("name", None)
+        zone = data.pop("zone", None)
+        region = data.pop("region", None)
+        mysql_id = data.pop("mysql_id", None)
+        provider_id = data.pop("provider_id", None)
+        result = self.resource.create(rid, name, provider_id, mysql_id,
+                                      zone, region, extend_info=data)
+
+        return {"result": result}
+
+
+class MysqlDatabaseDeleteController(BaseController):
+    name = "MysqlDatabase"
+    resource_describe = "MysqlDatabase"
+    allow_methods = ("POST",)
+    resource = MysqlDatabaseApi()
+
+    def before_handler(self, request, data, **kwargs):
+        validation.not_allowed_null(data=data,
+                                    keys=["id"]
+                                    )
+
+        validation.validate_string("id", data.get("id"))
+
+    def response_templete(self, data):
+        return {}
+
+    def main_response(self, request, data, **kwargs):
+        rid = data.pop("id", None)
+        result = self.resource.destory(rid)
+        return {"result": result}
