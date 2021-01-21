fe7bd0cd70f21add2427241e545404545ec5d30f
comprelzhang <comprelzhang@163.com>
2021-01-21 15:25:11 +0800

    [update] disk attach return

--- apps/controller/storage/disk_attach_controller.py
+++ apps/controller/storage/disk_attach_controller.py
@@ -59,10 +59,12 @@ class DiskAttachController(BackendController):
         extend_info = validation.validate_dict("extend_info", data.pop("extend_info", None))
 
         data.update(extend_info)
-        result = self.resource.attach(rid, name=name, provider_id=provider_id,
+        _, result = self.resource.attach(rid, name=name, provider_id=provider_id,
                                       disk_id=disk_id, instance_id=instance_id,
                                       zone=zone, region=region, extend_info=data)
-        return 1, result
+
+        res = {"id": rid, "resource_id": result.get("resource_id")}
+        return 1, res
 
 
 class DiskAttachIdController(BackendIdController):
@@ -116,11 +118,12 @@ class DiskAttachAddController(BaseController):
         instance_id = data.pop("instance_id", None)
         provider_id = data.pop("provider_id", None)
 
-        result = self.resource.attach(rid, name=name, provider_id=provider_id,
+        _, result = self.resource.attach(rid, name=name, provider_id=provider_id,
                                       disk_id=disk_id, instance_id=instance_id,
                                       zone=zone, region=region, extend_info=data)
 
-        return {"result": result}
+        res = {"id": rid, "resource_id": result.get("resource_id")}
+        return res
 
 
 class DiskDetachController(BaseController):
