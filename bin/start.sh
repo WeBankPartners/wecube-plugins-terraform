7499fffc4a923905533ca3b649777d0010ccefc2
comprelzhang <comprelzhang@163.com>
2021-01-25 14:44:40 +0800

    [add] use gunicorn start

--- bin/start.sh
+++ bin/start.sh
@@ -2,13 +2,15 @@
 
 # start wecube plugins terraform
 echo "terraform plugins starting "
+pidfile=bin/terraform.pid
+logfile=logs/err.log
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_PATH};
 
 mkdir ../logs
-PORT=`cat ../conf/application.conf | grep -v "^#" | grep serverport | awk -F "=" '{print $2}' | sed 's/ //g'`
-
-python ../manage.py runserver 0.0.0.0:$PORT >> ../logs/service.log
+cd ..
+echo "gunicorn starting process. wating ... "
+gunicorn  -c gunicorn.conf wecube_plugins_terraform.wsgi:application -t 900 --pid $pidfile --error-logfile $logfile --log-level info
 
 echo "done"
