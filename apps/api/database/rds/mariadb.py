0c24c01fc53c8a156b8104fd9467132fdac79dfa
comprelzhang <comprelzhang@163.com>
2021-02-03 16:46:24 +0800

    [update] move rds

--- apps/api/database/rds/mariadb.py
+++ apps/api/database/rds/mariadb.py
@@ -3,7 +3,7 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 from apps.background.resource.database.rds import MariaDBObject
-from .rds import RdsDBApi
+from apps.api.database.rds import RdsDBApi
 
 
 class MariaDBApi(RdsDBApi):
