e53f9113a4b3a001159b48ea9852981db9f4bc22
comprelzhang <comprelzhang@163.com>
2021-02-20 14:18:08 +0800

    [update] vpc subnet generate args

--- apps/api/network/subnet.py
+++ apps/api/network/subnet.py
@@ -8,7 +8,6 @@ from lib.logs import logger
 from lib.json_helper import format_json_dumps
 from core import local_exceptions
 from apps.common.convert_keys import define_relations_key
-from apps.api.configer.provider import ProviderApi
 from apps.api.apibase import ApiBase
 from apps.background.resource.resource_base import CrsObject
 
@@ -43,7 +42,7 @@ class SubnetApi(ApiBase):
         logger.info("before_keys_checks add info: %s" % (format_json_dumps(ext_info)))
         return ext_info
 
-    def generate_create_data(self, zone, create_data):
+    def generate_create_data(self, zone, create_data, **kwargs):
         r_create_data = {"vpc_id": create_data.get("vpc_id")}
         create_data = {"cidr": create_data.get("cidr"),
                        "name": create_data.get("name"),
@@ -51,6 +50,6 @@ class SubnetApi(ApiBase):
 
         return create_data, r_create_data
 
-    def generate_owner_data(self, create_data):
+    def generate_owner_data(self, create_data, **kwargs):
         owner_id = create_data.get("vpc_id")
         return owner_id, None
