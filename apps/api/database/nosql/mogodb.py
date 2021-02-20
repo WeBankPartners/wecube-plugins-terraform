1b69df1018f510f275494b1e9e144bb1be055b73
comprelzhang <comprelzhang@163.com>
2021-02-20 17:05:50 +0800

    [update] nosql create

--- apps/api/database/nosql/mogodb.py
+++ apps/api/database/nosql/mogodb.py
@@ -3,10 +3,10 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 from apps.background.resource.database.nosql import MongoDBObject
-from apps.api.database.rds import RdsDBApi
+from apps.api.database.nosql.nosql import NosqlApi
 
 
-class MongodbApi(RdsDBApi):
+class MongodbApi(NosqlApi):
     def __init__(self):
         super(MongodbApi, self).__init__()
         self.resource_name = "mongodb"
