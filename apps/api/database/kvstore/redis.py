ee2556f8aebf13a6ea180ebc5b420e40b3e8fa26
comprel <1079926688@qq.com>
2021-03-18 15:40:20 +0800

    [update] redis api

--- apps/api/database/kvstore/redis.py
+++ apps/api/database/kvstore/redis.py
@@ -17,6 +17,7 @@ class RedisApi(KvStoreApi):
         self.resource_name = "redis"
         self.resource_workspace = "redis"
         self._flush_resobj()
+        self.resource_keys_config = None
 
 
 class RedisBackupApi(KvBackupApi):
@@ -25,14 +26,50 @@ class RedisBackupApi(KvBackupApi):
         self.resource_name = "redis_backup"
         self.resource_workspace = "redis_backup"
         self._flush_resobj()
+        self.resource_keys_config = None
 
-    def before_keys_checks(self, provider, kvstore_id, is_update=None):
+    def before_keys_checks(self, provider, create_data, is_update=None):
         '''
 
         :param provider:
-        :param vpc_id:
+        :param create_data:
+        :param is_update:
         :return:
         '''
+
+        kvstore_id = create_data.get("redis_id")
+
+        self.resource_info(provider)
+        resource_property = self.resource_keys_config["resource_property"]
+        _kv_status = define_relations_key("redis_id", kvstore_id, resource_property.get("redis_id"))
+
+        ext_info = {}
+        if kvstore_id and (not _kv_status):
+            ext_info["redis_id"] = CrsObject("redis").object_resource_id(kvstore_id)
+
+        logger.info("before_keys_checks add info: %s" % (format_json_dumps(ext_info)))
+        return ext_info
+
+
+
+class RedisAccountApi(KvBackupApi):
+    def __init__(self):
+        super(RedisAccountApi, self).__init__()
+        self.resource_name = "redis_account"
+        self.resource_workspace = "redis_account"
+        self._flush_resobj()
+        self.resource_keys_config = None
+
+    def before_keys_checks(self, provider, create_data, is_update=None):
+        '''
+
+        :param provider:
+        :param create_data:
+        :param is_update:
+        :return:
+        '''
+
+        kvstore_id = create_data.get("redis_id")
 
         self.resource_info(provider)
         resource_property = self.resource_keys_config["resource_property"]
