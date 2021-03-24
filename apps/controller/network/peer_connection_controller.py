d12c665492947e34ac11ff7d69eb3662d8eca05d
comprel <1079926688@qq.com>
2021-03-24 14:40:13 +0800

    [add] 新增peer connection

---
+++ apps/controller/network/peer_connection_controller.py
@@ -0,0 +1,157 @@
+# _ coding:utf-8 _*_
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+from core import validation
+from core.controller import BackendController
+from core.controller import BackendIdController
+from core.controller import BaseController
+from lib.uuid_util import get_uuid
+from apps.api.network.peer_connection import PeerConnApi
+from apps.controller.source_controller import BaseSourceController
+
+
+class ResBase(object):
+    @classmethod
+    def allow_key(cls, data):
+        validation.allowed_key(data, ["id", "provider", "secret", "region", "zone",
+                                      "name", "vpc_id", "peer_vpc_id", "peer_region",
+                                      "extend_info"])
+
+    @classmethod
+    def not_null(cls, data):
+        validation.not_allowed_null(data=data,
+                                    keys=["region", "provider", "name", "vpc_id", "peer_vpc_id"]
+                                    )
+
+    @classmethod
+    def validate_keys(cls, data):
+        validation.validate_collector(data=data,
+                                      strings=["id", "name", "region", "zone",
+                                               "provider", "vpc_id", "secret",
+                                               "peer_vpc_id", "peer_region"],
+                                      dicts=["extend_info"])
+
+    @classmethod
+    def create(cls, resource, data, **kwargs):
+        rid = data.pop("id", None) or get_uuid()
+        secret = data.pop("secret", None)
+        region = data.pop("region", None)
+        zone = data.pop("zone", None)
+        provider = data.pop("provider", None)
+        name = data.pop("name", None)
+        vpc_id = data.pop("vpc_id", None)
+        peer_vpc_id = data.pop("peer_vpc_id", None)
+        peer_region = data.pop("peer_region", None)
+
+        extend_info = validation.validate_dict("extend_info", data.pop("extend_info", None))
+        data.update(extend_info)
+
+        create_data = {"name": name, "vpc_id": vpc_id,
+                       "peer_vpc_id": peer_vpc_id, "peer_region": peer_region}
+        _, result = resource.create(rid=rid, provider=provider,
+                                    region=region, zone=zone,
+                                    secret=secret,
+                                    create_data=create_data,
+                                    extend_info=data)
+
+        res = {"id": rid, "resource_id": str(result.get("resource_id"))[:64]}
+        return res, result
+
+
+class PeerConnController(BackendController):
+    allow_methods = ('GET', 'POST')
+    resource = PeerConnApi()
+
+    def list(self, request, data, orderby=None, page=None, pagesize=None, **kwargs):
+        '''
+
+        :param request:
+        :param data:
+        :param orderby:
+        :param page:
+        :param pagesize:
+        :param kwargs:
+        :return:
+        '''
+
+        validation.allowed_key(data, ["id", "provider", "region", 'resource_id',
+                                      "provider_id", "name", "enabled"])
+        return self.resource.resource_object.list(filters=data, page=page,
+                                                  pagesize=pagesize, orderby=orderby)
+
+    def before_handler(self, request, data, **kwargs):
+        ResBase.allow_key(data)
+        ResBase.not_null(data)
+        ResBase.validate_keys(data)
+
+    def create(self, request, data, **kwargs):
+        res, _ = ResBase.create(resource=self.resource, data=data)
+        return 1, res
+
+
+class PeerConnIdController(BackendIdController):
+    allow_methods = ('GET', 'DELETE',)
+    resource = PeerConnApi()
+
+    def show(self, request, data, **kwargs):
+        '''
+
+        :param request:
+        :param data:
+        :param kwargs:
+        :return:
+        '''
+
+        rid = kwargs.pop("rid", None)
+        return self.resource.resource_object.show(rid)
+
+    def delete(self, request, data, **kwargs):
+        rid = kwargs.pop("rid", None)
+        return self.resource.destory(rid)
+
+
+class PeerConnAddController(BaseController):
+    allow_methods = ("POST",)
+    resource = PeerConnApi()
+
+    def before_handler(self, request, data, **kwargs):
+        ResBase.not_null(data)
+        ResBase.validate_keys(data)
+
+    def response_templete(self, data):
+        return {}
+
+    def main_response(self, request, data, **kwargs):
+        res, _ = ResBase.create(resource=self.resource, data=data)
+        return res
+
+
+class PeerConnDeleteController(BaseController):
+    name = "PeerConn"
+    resource_describe = "PeerConn"
+    allow_methods = ("POST",)
+    resource = PeerConnApi()
+
+    def before_handler(self, request, data, **kwargs):
+        validation.not_allowed_null(data=data,
+                                    keys=["id"]
+                                    )
+
+        validation.validate_string("id", data.get("id"))
+
+    def response_templete(self, data):
+        return {}
+
+    def main_response(self, request, data, **kwargs):
+        rid = data.pop("id", None)
+        result = self.resource.destory(rid)
+        return {"result": result}
+
+
+class PeerConnSourceController(BaseSourceController):
+    name = "PeerConn"
+    resource_describe = "PeerConn"
+    allow_methods = ("POST",)
+    resource = PeerConnApi()
+
