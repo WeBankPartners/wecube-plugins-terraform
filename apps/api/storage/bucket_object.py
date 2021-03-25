61c460196120e1eb9f82693c85027dc5d9ba2edc
comprel <1079926688@qq.com>
2021-03-25 16:37:05 +0800

    [update]更新bucket 对象存储resource

--- apps/api/storage/bucket_object.py
+++ apps/api/storage/bucket_object.py
@@ -62,7 +62,7 @@ class BucketObjectApi(ApiBase):
         self.resource_info(provider)
         resource_values_config = self.values_config(provider)
 
-        resource_name = self.resource_keys_config["property"]
+        resource_name = self.resource_keys_config["resource_type"]
         resource_property = self.resource_keys_config["resource_property"]
         resource_extend_info = self.resource_keys_config["extend_info"]
 
