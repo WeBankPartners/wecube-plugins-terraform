1e3aa2f233a15dc67d99d79b74b25638c7b0487a
comprelzhang <comprelzhang@163.com>
2021-05-27 16:41:23 +0800

    [update]更新lb controller

--- apps/controller/loadbalance/lb_controller.py
+++ apps/controller/loadbalance/lb_controller.py
@@ -9,7 +9,9 @@ from core.controller import BaseController
 from lib.uuid_util import get_uuid
 from apps.api.loadbalance.lb import LBApi
 from apps.api.loadbalance.lb import LBBackendApi
-from apps.controller.source_controller import BaseSourceController
+from apps.controller.backend_controller import BackendAddController
+from apps.controller.backend_controller import BackendDeleteController
+from apps.controller.backend_controller import BackendSourceController
 
 
 class ResBase(object):
@@ -140,45 +142,19 @@ class LBIdDettachController(BackendIdController):
         return self.resource.destroy(rid)
 
 
-class LBAddController(BaseController):
+class LBAddController(BackendAddController):
     allow_methods = ("POST",)
     resource = LBBackendApi()
 
-    def before_handler(self, request, data, **kwargs):
-        ResBase.not_null(data)
-        ResBase.validate_keys(data)
 
-    def response_templete(self, data):
-        return {}
-
-    def main_response(self, request, data, **kwargs):
-        res, _ = ResBase.create(resource=self.resource, data=data)
-        return res
-
-
-class LBDeleteController(BaseController):
+class LBDeleteController(BackendDeleteController):
     name = "LB"
     resource_describe = "LB"
     allow_methods = ("POST",)
     resource = LBBackendApi()
 
-    def before_handler(self, request, data, **kwargs):
-        validation.not_allowed_null(data=data,
-                                    keys=["id"]
-                                    )
 
-        validation.validate_string("id", data.get("id"))
-
-    def response_templete(self, data):
-        return {}
-
-    def main_response(self, request, data, **kwargs):
-        rid = data.pop("id", None)
-        result = self.resource.destroy(rid)
-        return {"result": result}
-
-
-class LBSourceController(BaseSourceController):
+class LBSourceController(BackendSourceController):
     name = "LB"
     resource_describe = "LB"
     allow_methods = ("POST",)
