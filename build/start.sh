d98fe35ebdd13557d37d58b4d5b6e56d178c914b
zgyzgyhero <894526647@qq.com>
2021-08-06 21:55:43 +0800

    Update register xml

--- build/start.sh
+++ build/start.sh
@@ -11,5 +11,6 @@ sed -i "s~{{JWT_SIGNING_KEY}}~$JWT_SIGNING_KEY~g" /app/terraform/conf/default.js
 sed -i "s~{{SUB_SYSTEM_CODE}}~$SUB_SYSTEM_CODE~g" /app/terraform/conf/default.json
 sed -i "s~{{SUB_SYSTEM_KEY}}~$SUB_SYSTEM_KEY~g" /app/terraform/conf/default.json
 sed -i "s~{{PLUGIN_VERSION}}~$PLUGIN_VERSION~g" /app/terraform/conf/default.json
+sed -i "s~{{TERRAFORM_SECRET_KEY_SEED}}~$TERRAFORM_SECRET_KEY_SEED~g" /app/terraform/conf/default.json
 
 ./terraform-server
