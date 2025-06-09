d384db4a115a6a7b4f8ae05b07bd57cb868a9627
wanghao7717 <792974788@qq.com>
2025-06-09 15:52:53 +0800

    feat: 国际化问题还原

--- ui/src/locale/i18n/index.js
+++ ui/src/locale/i18n/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: wanghao7717 792974788@qq.com
+ * @Date: 2025-02-08 19:50:40
+ * @LastEditors: wanghao7717 792974788@qq.com
+ * @LastEditTime: 2025-06-09 15:52:18
+ * @FilePath: \wecube-plugins-terraform\ui\src\locale\i18n\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
@@ -9,18 +17,12 @@ import EN from './en-US.json'
 
 Vue.use(VueI18n)
 
-const i18n = new VueI18n({
-  locale: 'zh-CN',
-  messages: {
-    'zh-CN': {
-      ...zh,
-      ...ZH
-    },
-    'en-US': {
-      ...en,
-      ...EN
-    }
-  }
-})
+Vue.locale('zh-CN', Object.assign(zh, ZH))
+Vue.locale('en-US', Object.assign(en, EN))
 
-export default i18n
+const navLang = navigator.language
+
+const localLang = navLang === 'zh-CN' || navLang === 'en-US' ? navLang : false
+
+const lang = window.localStorage.getItem('lang') || localLang || 'en-US'
+Vue.config.lang = lang
--- ui/src/main.js
+++ ui/src/main.js
@@ -1,17 +1,32 @@
+/*
+ * @Author: wanghao7717 792974788@qq.com
+ * @Date: 2025-02-08 19:50:40
+ * @LastEditors: wanghao7717 792974788@qq.com
+ * @LastEditTime: 2025-06-09 15:52:02
+ * @FilePath: \wecube-plugins-terraform\ui\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import 'regenerator-runtime/runtime'
 import ViewUI from 'view-design'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import i18n from './locale/i18n'
+// import 'view-design/dist/styles/iview.css'
+import locale from 'view-design/dist/locale/en-US'
+import VueI18n from 'vue-i18n'
+import './locale/i18n'
 import './styles/index.less'
 
 Vue.config.productionTip = false
 
-Vue.use(ViewUI)
+Vue.use(ViewUI, {
+  transfer: true,
+  size: 'default',
+  VueI18n,
+  locale
+})
 
 new Vue({
   router,
-  i18n,
   render: h => h(App)
 }).$mount('#app')
