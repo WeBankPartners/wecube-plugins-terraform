75c77d032653a94f3facf8495d5c957fccc87a61
wanghao7717 <792974788@qq.com>
2025-06-09 15:33:15 +0800

    feat: 国际化问题修复

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,22 +3,15 @@ import ViewUI from 'view-design'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import 'view-design/dist/styles/iview.css'
-import locale from 'view-design/dist/locale/en-US'
-import VueI18n from 'vue-i18n'
-import './locale/i18n'
+import i18n from './locale/i18n'
 import './styles/index.less'
 
 Vue.config.productionTip = false
 
-Vue.use(ViewUI, {
-  transfer: true,
-  size: 'default',
-  VueI18n,
-  locale
-})
+Vue.use(ViewUI)
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
