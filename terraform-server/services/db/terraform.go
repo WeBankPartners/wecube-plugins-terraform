56fed3c1a2a961b1a2c286d65aaa4c0eb42855c9
zgyzgyhero <894526647@qq.com>
2025-06-19 13:38:00 +0800

    update convert param with empty list

--- terraform-server/services/db/terraform.go
+++ terraform-server/services/db/terraform.go
@@ -4321,11 +4321,15 @@ func handleConvertParams(action string,
 		}
 
 		if tfArgumentList[i].IsMulti == "Y" && tfArgumentList[i].IsNull == "Y" {
-			log.Logger.Info("handleConvertParams", log.String("name", tfArgumentList[i].Name), log.String("value", fmt.Sprintf("%v", arg)))
+			tmpArgString := fmt.Sprintf("%v", arg)
+			log.Logger.Debug("handleConvertParams", log.String("name", tfArgumentList[i].Name), log.String("value", tmpArgString))
+			if tmpArgString == "[]" {
+				continue
+			}
 		}
 
 		if arg == nil || arg == "" {
-			log.Logger.Info("handleConvertParams continue arg", log.String("name", tfArgumentList[i].Name))
+			log.Logger.Debug("handleConvertParams continue arg", log.String("name", tfArgumentList[i].Name))
 			continue
 		}
 
